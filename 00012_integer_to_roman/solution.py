6220a57a22f6c572036a4ad565a23ea44aa0e32f
matt <mattdgraham95@gmail.com>
2022-08-21 17:11:01 -0400

    00012 postsolution

--- 00012_integer_to_roman/solution.py
+++ 00012_integer_to_roman/solution.py
@@ -2,11 +2,17 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         value_dict = {
             "M": 1000,
+            "CM": 900,
             "D": 500,
+            "CD": 400,
             "C": 100,
+            "XC": 90,
             "L": 50,
+            "XL": 40,
             "X": 10,
+            "IX": 9,
             "V": 5,
+            "IV": 4,
             "I": 1,
         }
         buff = ""
@@ -14,15 +20,5 @@ class Solution:
             num_symbols = num // numeric
             buff += num_symbols * roman
             num = num % numeric
-            
-        # cleanup
-        buff = (buff
-            .replace("DCCCC", "CM")
-            .replace("CCCC", "CD")
-            .replace("LXXXX", "XC")
-            .replace("XXXX", "XL")
-            .replace("VIIII", "IX")
-            .replace("IIII", "IV")
-        )
-            
+
         return buff
