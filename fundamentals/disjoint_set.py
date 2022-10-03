b9b362611bea1b372da4fe209652e41ba8da9e57
matt <mattdgraham95@gmail.com>
2022-10-03 09:19:03 -0400

    optimized disjoint sets

--- fundamentals/disjoint_set.py
+++ fundamentals/disjoint_set.py
@@ -81,3 +81,57 @@ class QuickUnionByRank:
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
+
+
+class QuickUnionPathCompressionOptimization:
+    """
+    Quick Union implementation with path compression
+    """
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+
+    def find(self, x):
+        if self.root[x] == x:
+            return x
+        
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            self.root[rootY] = rootX
+
+    def connected(self, x, y ):
+        return self.find(x) == self.find(y)
+
+
+class QuickUnionOptimized:
+    """
+    QuickUnion with path compression and union by rank
+    """
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootY] > self.rank[rootX]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
